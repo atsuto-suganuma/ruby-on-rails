ec50662adf13a9e81eccc38d923bbc3804d0b04b
atsuto suganuma <86strikepackage@gmail.com>
2019-07-11 15:37:54 +0900

    [finish 3-8]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3e7fd5489c5362b2e5f9780a8698c3a29a54261bfaf5bf1c23567bed6def707839c4d43ff55cb125ec0d1b030e8c5ab694b033045c062fc45944f9a882c4934c'
